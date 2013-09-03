97995e2df128caf7bf6f0368fd86aff1abfdd085
greenmachine1 <scary_penguins05@hotmail.com>
2013-09-02 21:05:40 -0700

    implemented my network connection type detection

--- Week_1_project/src/com/example/week_1_project/MainActivity.java
+++ Week_1_project/src/com/example/week_1_project/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.week_1_project;
 
+import com.example.lib.WebInfo;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -11,9 +15,18 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+        // creating my global variables
+        Context _context;
+        Boolean _connected = false;
         
+        _context = this;
         
-        
+        // Detect network connection
+        _connected = WebInfo.getConnectionStatus(_context);
+         if(_connected)
+         {
+        	 Log.i("Network Connection", WebInfo.getConnectionType(_context));
+         }
         
         
         
