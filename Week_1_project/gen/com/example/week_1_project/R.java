c74570de825ae076e31c196ca5aa70f0be944ff4
greenmachine1 <scary_penguins05@hotmail.com>
2013-09-02 20:50:51 -0700

    added more to my xml layout

--- Week_1_project/gen/com/example/week_1_project/R.java
+++ Week_1_project/gen/com/example/week_1_project/R.java
@@ -24,7 +24,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int Name=0x7f080002;
+        public static final int action_settings=0x7f080005;
+        public static final int genre=0x7f080003;
+        public static final int goButton=0x7f080001;
+        public static final int url=0x7f080004;
+        public static final int userEnteredText=0x7f080000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
