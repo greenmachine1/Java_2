1b8688c5691d381b029fa6e0386caa815454c357
greenmachine1 <scary_penguins05@hotmail.com>
2013-09-14 12:45:46 -0700

    still more work on my content provider class

--- Week_2_project/src/com/Cory/week_2_project/CollectionProvider.java
+++ Week_2_project/src/com/Cory/week_2_project/CollectionProvider.java
@@ -2,27 +2,72 @@ package com.Cory.week_2_project;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.MatrixCursor;
 import android.net.Uri;
+import android.provider.BaseColumns;
 
 public class CollectionProvider extends ContentProvider{
 
+	FileManager m_file;
+    String fileName = "returned_json.txt";
+	
+	public static final String AUTHORITY = "com.Cory.week_2_project.collectionprovider";
+	
+	public static class itunesData implements BaseColumns{
+		
+		public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/items");
+		
+		public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd.Cory.week_2_project.item";
+		public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd.Cory.week_2_project.item";
+		
+		// define colums
+		public static final String ARTIST_COLUMN = "artist";
+		public static final String GENRE_COLUMN = "genre";
+		public static final String URL_COLUMN = "url";
+		
+		public static final String[] PROJECTION = {"_Id, ARTIST_COLUMN, GENRE_COLUMN, URL_COLUMN"};
+		
+		private itunesData() {};
+	}
+	
+	public static final int ITEMS = 1;
+	public static final int ITEMS_ID = 2;
+	
+	private static final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+	
+	static {
+		uriMatcher.addURI(AUTHORITY, "items/", ITEMS);
+		uriMatcher.addURI(AUTHORITY, "items/#", ITEMS_ID);
+	}
+	
 	@Override
 	public int delete(Uri arg0, String arg1, String[] arg2) {
 		// TODO Auto-generated method stub
-		return 0;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
-	public String getType(Uri arg0) {
+	public String getType(Uri uri) {
 		// TODO Auto-generated method stub
+		
+		switch(uriMatcher.match(uri)){
+		case ITEMS:
+			return itunesData.CONTENT_TYPE;
+			
+		
+		case ITEMS_ID:
+			return itunesData.CONTENT_ITEM_TYPE;
+		
+		}
 		return null;
 	}
 
 	@Override
 	public Uri insert(Uri arg0, ContentValues arg1) {
 		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
@@ -32,16 +77,29 @@ public class CollectionProvider extends ContentProvider{
 	}
 
 	@Override
-	public Cursor query(Uri arg0, String[] arg1, String arg2, String[] arg3,
-			String arg4) {
-		// TODO Auto-generated method stub
+	public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
+			String sortOrder) {
+		
+		MatrixCursor result = new MatrixCursor(itunesData.PROJECTION);
+		
+		String JSONStorage = m_file.readStringFile(getContext(), fileName);
+		
+		switch(uriMatcher.match(uri)){
+		case ITEMS:
+			return itunesData.CONTENT_TYPE;
+			
+		
+		case ITEMS_ID:
+			return itunesData.CONTENT_ITEM_TYPE;
+		
+		}
 		return null;
 	}
 
 	@Override
 	public int update(Uri arg0, ContentValues arg1, String arg2, String[] arg3) {
 		// TODO Auto-generated method stub
-		return 0;
+		throw new UnsupportedOperationException();
 	}
 
 }
