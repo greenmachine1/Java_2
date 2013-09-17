ee3de554574cf0b1ea607c098842f9bee0f9025a
greenmachine1 <scary_penguins05@hotmail.com>
2013-09-16 23:15:05 -0700

    added all my files

--- Week_3_project/src/com/Cory/week_3_project/MainActivity.java
+++ Week_3_project/src/com/Cory/week_3_project/MainActivity.java
@@ -1,22 +1,189 @@
 package com.Cory.week_3_project;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.Cory.week_3_project.FileManager;
+import com.Cory.lib.WebInfo;
+
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Messenger;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+	// creating my global variables
+    Context _context;
+    Boolean _connected = false;
+    TextView text;
+    ListView listView;
+    
+    FileManager m_file;
+    String fileName = "returned_json.txt";
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+       
+        _context = this;
+        
+        text = (TextView)findViewById(R.id.resultText);
+        
+        // targetting my listView
+        listView = (ListView)this.findViewById(R.id.list);
+        View listHeader = this.getLayoutInflater().inflate(R.layout.list_header, null);
+        listView.addHeaderView(listHeader);
+        
+        
+        // Detect network connection
+        _connected = WebInfo.getConnectionStatus(_context);
+         if(_connected)
+         {
+        	 Log.i("Network Connection", WebInfo.getConnectionType(_context));
+         }
+         
+         // basically this is the cache data that gets displayed if internet connection is
+         // not available
+         else {
+        	 if(m_file.readStringFile(_context, fileName) != null){
+        		 String stringToDisplay = m_file.readStringFile(_context, fileName);
+        		 //text.setText(stringToDisplay);
+        	 }
+        	 else{
+        		 return;
+        	 }
+         }
+         
+         // creation of my go button
+         Button button = (Button) findViewById(R.id.goButton);
+         button.setOnClickListener(new OnClickListener() {
+         
+        	 // what happens when button is clicked!
+			@Override
+			public void onClick(View v) {
+				
+				// targetting the main edit text box
+				EditText userEnteredField = (EditText) findViewById(R.id.userEnteredText);
+				//userEnteredField.setText(CollectionProvider.itunesData.CONTENT_URI.toString());
+				
+				String inputString = userEnteredField.getText().toString();
+				
+				Log.i("User input", inputString);
+				
+				// my handler.  Handles the return info
+				final Handler JsonHandler = new Handler(){
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+					@Override
+					public void handleMessage(Message message){
+						
+						// what gets returned from the called service
+						Object returnedObject = message.obj;
+						
+						// casting the object to a string
+						String returnedObjectString = returnedObject.toString();
+						
+						if(message.arg1 == RESULT_OK && returnedObject != null){
+							
+							// calls on my FileManager class
+					        m_file = FileManager.getInstance();
+					        m_file.writeStringFile(_context, fileName, returnedObjectString);
+							
+							//text.setText(m_file.readStringFile(_context, fileName));
+							
+							//Log.i("object", returnedObjectString);
+					        
+					        displayData();
+						}
+					}
+		    		
+		    	}; 
+				
+		    	
+		    	
+		    	
+				
+				// creation of my messenger to the service
+		    	Messenger jsonMessenger = new Messenger(JsonHandler);
+				
+				Intent myServiceIntent = new Intent(_context, JsonService.class);
+				
+				// basically this passes info to my service
+				myServiceIntent.putExtra("messenger", jsonMessenger);
+				myServiceIntent.putExtra("key", inputString);
+				startService(myServiceIntent);
+			}
+		});
+        
+        
+    }
+    
+    public void displayData(){
+    	// loading my file into a string
+    	String JSONString = m_file.readStringFile(this, fileName);
+    	
+    	ArrayList<HashMap<String, String>>mylist = new ArrayList<HashMap<String,String>>();
+    	JSONObject job = null;
+    	JSONArray results = null;
+    	
+    	try{
+    		
+    		// getting the array from the field "results"
+    		job = new JSONObject(JSONString);
+    		results = job.getJSONArray("results");
+    		
+    		// gathers the specific fields
+    		String artistName = results.getJSONObject(0).getString("artistName").toString();
+    		String artistGenre = results.getJSONObject(0).getString("primaryGenreName").toString();
+    		String artistURL = results.getJSONObject(0).getString("artistLinkUrl").toString();
+    		
+    		//text.setText("artistName: " + artistName + "artistGenre: " + artistGenre + "artistURL: " + artistURL);
+    		HashMap<String, String> displayMap = new HashMap<String, String>();
+    		displayMap.put("artist", artistName);
+    		displayMap.put("genre", artistGenre);
+    		displayMap.put("url", artistURL);
+    		
+    		//displayMap.put("artist", cursor.getString(1));
+    		
+    		mylist.add(displayMap);
+    		
+    		// this is complicated but it basically assigns the rows for each element
+    		SimpleAdapter adapter = new SimpleAdapter(this, mylist, R.layout.list_row, 
+    				new String[] {"artist", "genre", "url"}, 
+    				new int[] {R.id.artist, R.id.genre, R.id.url});
+    		
+    		listView.setAdapter(adapter);
+    		
+    	} catch(Exception e){
+    		
+    	}
+    	
+    	
+    }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
 }
