12e0846e0ea9091516694f43e31c3055bce0730a
greenmachine1 <scary_penguins05@hotmail.com>
2013-09-19 12:22:39 -0700

    added 2 new xml files

--- Week_3_final_project/gen/com/Cory/week_3_final_project/R.java
+++ Week_3_final_project/gen/com/Cory/week_3_final_project/R.java
@@ -24,18 +24,26 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
-        public static final int bandInfo=0x7f080004;
-        public static final int goPulledInfo=0x7f080005;
-        public static final int mainText=0x7f080001;
-        public static final int searchText=0x7f080002;
+        public static final int action_settings=0x7f08000c;
+        public static final int artist=0x7f080005;
+        public static final int artist_header=0x7f080002;
+        public static final int bandInfo=0x7f08000a;
+        public static final int genre=0x7f080006;
+        public static final int genre_header=0x7f080003;
+        public static final int goPulledInfo=0x7f08000b;
+        public static final int list=0x7f080001;
+        public static final int searchText=0x7f080008;
         public static final int topText=0x7f080000;
-        public static final int userInput=0x7f080003;
+        public static final int url=0x7f080007;
+        public static final int url_header=0x7f080004;
+        public static final int userInput=0x7f080009;
     }
     public static final class layout {
         public static final int bandinfo=0x7f030000;
-        public static final int main=0x7f030001;
-        public static final int pulledinfo=0x7f030002;
+        public static final int list_header=0x7f030001;
+        public static final int list_row=0x7f030002;
+        public static final int main=0x7f030003;
+        public static final int pulledinfo=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- Week_3_final_project/src/com/Cory/week_3_final_project/BandInfo.java
+++ Week_3_final_project/src/com/Cory/week_3_final_project/BandInfo.java
@@ -1,12 +1,27 @@
 package com.Cory.week_3_final_project;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import com.Cory.lib.WebInfo;
+import com.Cory.week_3_final_project.JsonService;
+import com.Cory.week_3_final_project.R;
 import com.Cory.week_3_final_project.SaveData;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Messenger;
 import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 
@@ -27,6 +42,8 @@ public class BandInfo extends Activity {
 	// for my save portion
 	SaveData m_file;
     String fileName = "returned_json.txt";
+    
+    ListView listView;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
@@ -43,26 +60,105 @@ public class BandInfo extends Activity {
          }
 		
          
+      // targetting my listView
+         listView = (ListView)this.findViewById(R.id.list);
+         View listHeader = this.getLayoutInflater().inflate(R.layout.list_header, null);
+         listView.addHeaderView(listHeader);
+         
 		// getting the user information through the getIntent method
 		Bundle data = getIntent().getExtras();
 		if(data != null){
 			String newString = data.getString("userInput");
 			
+			final Handler JsonHandler = new Handler(){
+				
+				@Override
+				public void handleMessage(Message message){
+					
+					// what gets returned from the called service
+					Object returnedObject = message.obj;
+					
+					// casting the object to a string
+					String returnedObjectString = returnedObject.toString();
+					
+					if(message.arg1 == RESULT_OK && returnedObject != null){
+						
+						// calls on my FileManager class
+				        m_file = SaveData.getInstance();
+				        m_file.writeStringFile(_context, fileName, returnedObjectString);
+						
+						//text.setText(m_file.readStringFile(_context, fileName));
+						
+						//Log.i("object", returnedObjectString);
+				        
+				        displayData();
+					}
+				}
+
+				
+			};
+			
+			// creation of my messenger to the service
+	    	Messenger jsonMessenger = new Messenger(JsonHandler);
+			
+			Intent myServiceIntent = new Intent(_context, JsonService.class);
+			
+			// basically this passes info to my service
+			myServiceIntent.putExtra("messenger", jsonMessenger);
+			myServiceIntent.putExtra("key", newString);
+			startService(myServiceIntent);
 			
 			
-			
-			
-			
-			
-			
-			
+		
 			// this is the output section
 			topTextBox = (TextView)findViewById(R.id.topText);
 			topTextBox.setText("Here are the results for " + newString);
 			
-			mainTextBox = (TextView)findViewById(R.id.mainText);
+			//mainTextBox = (TextView)findViewById(R.id.mainText);
 			
 			Log.i("user", "" + newString);
 		}
+	}
+	
+	public void displayData(){
+    	// loading my file into a string
+    	String JSONString = m_file.readStringFile(this, fileName);
+    	
+    	ArrayList<HashMap<String, String>>mylist = new ArrayList<HashMap<String,String>>();
+    	JSONObject job = null;
+    	JSONArray results = null;
+    	
+    	try{
+    		
+    		// getting the array from the field "results"
+    		job = new JSONObject(JSONString);
+    		results = job.getJSONArray("results");
+    		
+    		// gathers the specific fields
+    		String artistName = results.getJSONObject(0).getString("artistName").toString();
+    		String artistGenre = results.getJSONObject(0).getString("primaryGenreName").toString();
+    		String artistURL = results.getJSONObject(0).getString("artistLinkUrl").toString();
+    		
+    		//text.setText("artistName: " + artistName + "artistGenre: " + artistGenre + "artistURL: " + artistURL);
+    		HashMap<String, String> displayMap = new HashMap<String, String>();
+    		displayMap.put("artist", artistName);
+    		displayMap.put("genre", artistGenre);
+    		displayMap.put("url", artistURL);
+    		
+    		//displayMap.put("artist", cursor.getString(1));
+    		
+    		mylist.add(displayMap);
+    		
+    		// this is complicated but it basically assigns the rows for each element
+    		SimpleAdapter adapter = new SimpleAdapter(this, mylist, R.layout.list_row, 
+    				new String[] {"artist", "genre", "url"}, 
+    				new int[] {R.id.artist, R.id.genre, R.id.url});
+    		
+    		listView.setAdapter(adapter);
+    		
+    	} catch(Exception e){
+    		
+    	}
+    	
 	}
 }
