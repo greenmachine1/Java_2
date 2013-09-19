37c7007ea3fd938c04ca6cf968c23f14ad8b3abe
greenmachine1 <scary_penguins05@hotmail.com>
2013-09-19 12:56:34 -0700

    working on my pulled data activity

--- Week_3_final_project/src/com/Cory/week_3_final_project/PulledInfo.java
+++ Week_3_final_project/src/com/Cory/week_3_final_project/PulledInfo.java
@@ -1,12 +1,76 @@
 package com.Cory.week_3_final_project;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 public class PulledInfo extends Activity {
+	
+	
+	SaveData m_file;
+	String fileName = "returned_json.txt";
+	
+	ListView listView;
+	
+	
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.pulledinfo);
+		
+		// this needs to pull the json data from the stored file and seperate the needed info into columns
+		displayData();
+		
+		
+	}
+	
+	public void displayData(){
+		
+		String JSONString = m_file.readStringFile(this, fileName);
+    	
+    	ArrayList<HashMap<String, String>>mylist = new ArrayList<HashMap<String,String>>();
+    	JSONObject job = null;
+    	JSONArray results = null;
+    	
+    	try{
+    		
+    		// getting the array from the field "results"
+    		job = new JSONObject(JSONString);
+    		results = job.getJSONArray("results");
+    		
+    		// gathers the specific fields
+    		String artistName = results.getJSONObject(0).getString("artistName").toString();
+    		String artistGenre = results.getJSONObject(0).getString("primaryGenreName").toString();
+    		String artistURL = results.getJSONObject(0).getString("artistLinkUrl").toString();
+    		
+    		//text.setText("artistName: " + artistName + "artistGenre: " + artistGenre + "artistURL: " + artistURL);
+    		HashMap<String, String> displayMap = new HashMap<String, String>();
+    		displayMap.put("artist", artistName);
+    		displayMap.put("genre", artistGenre);
+    		displayMap.put("url", artistURL);
+    		
+    		//displayMap.put("artist", cursor.getString(1));
+    		
+    		mylist.add(displayMap);
+    		
+    		// this is complicated but it basically assigns the rows for each element
+    		SimpleAdapter adapter = new SimpleAdapter(this, mylist, R.layout.list_row, 
+    				new String[] {"artist", "genre", "url"}, 
+    				new int[] {R.id.artist, R.id.genre, R.id.url});
+    		
+    		listView.setAdapter(adapter);
+    		
+    	} catch(Exception e){
+    		
+    	}
+    	
 	}
 }
