214e866fe907ce4bb5127640cacb00f9c351f14d
greenmachine1 <scary_penguins05@hotmail.com>
2013-09-18 20:13:44 -0700

    working more on my buttons

--- Week_3_final_project/src/com/Cory/week_3_final_project/MainActivity.java
+++ Week_3_final_project/src/com/Cory/week_3_final_project/MainActivity.java
@@ -3,6 +3,9 @@ package com.Cory.week_3_final_project;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,33 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		
+		// button for my band info retrieval
+		Button bandInfoButton = (Button)findViewById(R.id.bandInfo);
+		bandInfoButton.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				
+				
+			}
+			
+		});
+		
+		// button for my pulled info 
+		Button pulledInfoButton = (Button)findViewById(R.id.goPulledInfo);
+		pulledInfoButton.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+		
+		
+		
 	}
 
 	@Override
